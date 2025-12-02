c3086ebb5597e37e5b31b041fae49f753e2f722b
nalugao <nalugao@hotmail.com>
2025-12-02 16:21:31 -0300

    add scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>A vida acontece depois do medo. Vá em frente!</h1>
+  return <h1>A vida acontece depois do medo. Vá em frente!</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identação errada");
 }
 
 export default Home;
